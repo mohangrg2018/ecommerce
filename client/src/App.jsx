9525f0df01904bfd9e8530b0d3e6b51b3ff84c37
mohangrg2018 <mohangrg2030@gmail.com>
2025-07-18 12:49:01 +0545

    react setup done, tailwindcss and react-router-dom installed

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import Product from "./pages/Product";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
